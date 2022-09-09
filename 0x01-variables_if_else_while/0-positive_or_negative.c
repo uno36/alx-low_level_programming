bc492e7e32044ea8e6a6cb37e822bb907be34be3
emmanuel tofa <emmanuelutofa@gmail.com>
2022-09-09 05:58:11 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -20,13 +20,14 @@ int main(void)
 
         }
 	
-	else if (n==0) {
+	else if (n == 0) {
 	  printf("%d is zero\n", n);
 	}
 	
-	else
+	else {
 	  printf("%d is negative\n", n);
         }
 
 	return (0);
 }
+
