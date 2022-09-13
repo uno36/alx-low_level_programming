73e88fe9632424a39fd6d2b804688167aff00bd8
emmanuel tofa <emmanuelutofa@gmail.com>
2022-09-13 11:55:01 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,25 +1,21 @@
 #include "main.h"
-
 /**
- * main - check the code.
+ * print_alphabet_x10 - entry point
  *
- * Return: Always 0.
+ * Return: Nothing to return
  */
-int main(void)
-{
-print_alphabet_x10();
 
-char c;
-int i;
-i = 0;
-while (i < 10)
+void print_alphabet_x10(void)
 {
-for (c = 'a'; c <= 'z'; c++)
+int i = 1;
+while (i <= 10)
 {
-_putchar(c);
+char j;
+for (j = 'a'; j <= 'z'; j++)
+{
+_putchar(j);
 }
 _putchar('\n');
 i++;
 }
-return (0);
 }
