40bca0e87d8911d31ac29af6c7df48b8dbd819c3
emmanuel tofa <emmanuelutofa@gmail.com>
2022-09-13 14:26:41 -0700

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,50 +1,77 @@
 #include "main.h"
 
 /**
-  * print_times_table - Prints a multiplication table up to param
-  * @n: The number to be treated
-  *
-  * Return: Number matrix
-  */
+ * displayNumber - display a number
+ *
+ * @number: number to show
+ *
+ * Return: int - result of addition
+ */
+void displayNumber(int number)
+{
+	int lastDigit;
+
+	if (number < 0)
+	{
+	_putchar('-');
+	number *= -1;
+	}
+
+	lastDigit = number % 10;
+
+	if (number >= 10)
+	{
+	number /= 10;
+	displayNumber(number);
+	}
+
+	_putchar(lastDigit + '0');
+}
+
+/**
+ * squareTable - display a square table
+ *
+ * @n: square number
+ */
+void squareTable(int n)
+{
+	int column, row, number;
+
+	for (row = 0; row <= n; row++)
+	{
+	for (column = 0; column <= n; column++)
+	{
+	number = column * row;
+
+	if (column > 0 && number >= 0 && number < 10)
+	{
+	_putchar(' ');
+	}
+	if (column > 0 && number >= 0 && number < 100)
+	{
+	_putchar(' ');
+	}
+	displayNumber(number);
+	if (column < n)
+	{
+	_putchar(',');
+	_putchar(' ');
+	}
+	}
+
+	_putchar('\n');
+	}
+}
+
+/**
+ * print_times_table - display n square table
+ *
+ * @n: square table number
+ */
 void print_times_table(int n)
 {
-int x, y, z;
-
-if (n >= 0 && n <= 14)
-{
-for (x = 0; x <= n; x++)
-{
-for (y = 0; y <= n; y++)
-{
-z = x * y;
-if (z > 99)
-{
-_putchar(',');
-_putchar(32);
-_putchar((z / 100) + '0');
-_putchar(((z / 10) % 10) + '0');
-_putchar((z % 10) + '0');
-}
-else if (z > 9)
-{
-_putchar(',');
-_putchar(32);
-_putchar(32);
-_putchar(((z / 10) % 10) + '0');
-_putchar((z % 10) + '0');
-}
-else
-{
-if (y != 0)
-{
-_putchar(',');
-_putchar(32);
-_putchar(32);
-_putchar(32);
-}
-_putchar(z + '0');
-}
-}
-_putchar('\n');
-}
+	if (n <= 15 && n >= 0)
+	{
+	squareTable(n);
+	}
 }
