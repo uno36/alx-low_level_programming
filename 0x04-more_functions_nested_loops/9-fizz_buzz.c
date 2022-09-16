5e88bedafd565aa62ea8c4e0190441d1b2dead10
emmanuel tofa <emmanuelutofa@gmail.com>
2022-09-15 17:35:13 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,35 +10,19 @@
 
 int main(void)
 {
-	int num = 1;
+int i;
 
-	while (num++ < 100)
-	{
-		if ((num % 3 == 0) && (num % 5 == 0))
-		{
-			printf("FizzBuzz ");
-		}
-		else if ((num % 3) == 0)
-		{
-			printf("Fizz ");
-		}
-		else if ((num % 5) == 0)
-		{
-			if (num != 100)
-			{
-				printf("Buzz ");
-			}
-			else
-			{
-				printf("Buzz");
-			}
-		}
-		else
-		{
-			printf("%d ", num);
-		}
-	}
-	printf("\n");
-
-	return (0);
+for (i = 1; i <= 100; i++)
+{
+if (i % 15 == 0)
+printf("FizzBuzz");
+else if ((i % 3) == 0)
+printf("Fizz");
+else if ((i % 5) == 0)
+printf("Buzz");
+else
+printf("%d", i);
+printf(" ");
+}
+return (0);
 }
