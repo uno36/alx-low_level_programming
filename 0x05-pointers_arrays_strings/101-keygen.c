ee663c1a2d697df8f9b20df899ba9607e3c0ad44
emmanuel tofa <emmanuelutofa@gmail.com>
2022-09-19 02:38:02 -0700

    Don't hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+int main(void ){
+
+    int length;
+    printf("Enter expected length of password: ");
+    scanf("%d", &length);
+
+    if(length < 0){
+        printf("Password length must be >= 1!");
+        return 1;
+    }
+
+    char *password = malloc(length + 1);
+    char *digits = "0123456789";
+    int digits_length =strlen (digits);
+
+    char *lowers = "abcdefghijklmnopqrstuvwxyz";
+    int lowers_length = strlen(lowers);
+
+    char *uppers = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    int uppers_length = strlen(uppers);
+
+    char *symbols = "!@#$%^&*()";
+    int symbols_length = strlen(symbols);
+
+    srand(time(NULL) * getpid());
+    for(int i = 0; i< length; i++){
+        int char_type = rand() % 4;
+
+        if(char_type == 0)
+            password[i] = digits[rand() % digits_length];
+        else if (char_type == 1 )
+            password[i] = lowers[rand() % lowers_length];
+        else if (char_type == 2)
+            password[i] = uppers[rand() % uppers_length];
+        else
+            password[i] = symbols[rand() % symbols_length];
+
+    }
+
+    password[length] = '\0';
+    printf("password: %s\n", password);
+    free(password);
+
+    return(0);
+}
