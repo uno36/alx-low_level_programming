95102a6b3688859baa7fefd34fa50c99daac6340
emmanuel tofa <emmanuelutofa@gmail.com>
2022-09-21 01:12:34 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+char *_strcat (char *dest, char *scr);
+char *_strncat (char *dest, char *scr, int n);
+char *_strncpy (char *dest, char *scr, int n);
+int _strcmp (char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *rot13(char *);
+char *leet(char *);
+void print_number(int n);
+int _putchar(char c);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
