aed0c7e0a80d16fae509466b82f2b3ec86b80912
emmanuel tofa <emmanuelutofa@gmail.com>
2022-09-22 18:21:00 -0700

    It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,116 +1,48 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: buffer for result
+ * @size_r: buffer size
  *
- * @s: char pointer
- *
- * Return: Size of the string
- */
-int _strlen(char *s)
-{
-	int size = 0;
-
-	while (*s != '\0')
-	{
-	size++;
-	s++;
-	}
-
-	return (size);
-}
-
-/**
- * isDigit - check if a character is a digit
- *
- * @c: character
- *
- * Return: true if successful
- */
-int isDigit(char c)
-{
-	return (c >= '0' && c <= '9');
-}
-
-/**
- * getCharN - get character at N position
- *
- * @c: character
- * @n: position
- *
- * Return: character
- */
-char getCharN(char *c, int n)
-{
-	int size = _strlen(c);
-	char ch = (size >= n) ? (c[size - 1 - n]) : '0';
-
-	return (isDigit(ch) ? ch : '0');
-}
-
-/**
- * addChar - add char digit
- *
- * @c1: character
- * @c2: position
- *
- * Return: number
- */
-int addChar(char c1, char c2)
-{
-	return ((c1 - 48) + (c2 - 48));
-}
-
-/**
- * infinite_add - add two char number
- *
- * @n1: character number
- * @n2: char number
- * @r: buffer
- * @size_r: size of the buffer
- *
- * Return: result
+ * Return: address of r or 0
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int cLoop, rcLoop, result, next = 0;
-	char temp[size_r], c1, c2;
+	int i, j, k, l, m, n;
 
-	for (cLoop = 0; cLoop < size_r; cLoop++)
+	for (i = 0; n1[i]; i++)
+		;
+	for (j = 0; n2[j]; j++)
+		;
+	if (i > size_r || j > size_r)
+		return (0);
+	m = 0;
+	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 	{
-	temp[cLoop] = '\0';
-	r[cLoop] = '\0';
+		n = m;
+		if (i >= 0)
+			n += n1[i] - '0';
+		if (j >= 0)
+			n += n2[j] - '0';
+		if (i < 0 && j < 0 && n == 0)
+		{
+			break;
+		}
+		m = n / 10;
+		r[k] = n % 10 + '0';
 	}
-	for (cLoop = 0; cLoop < size_r; cLoop++)
+	r[k] = '\0';
+	if (i >= 0 || j >= 0 || m)
+		return (0);
+	for (k -= 1, l = 0; l < k; k--, l++)
 	{
-	c1 = getCharN(n1, cLoop);
-	c2 = getCharN(n2, cLoop);
-	result = (((temp[cLoop] == '\0') ? '0' : temp[cLoop]) - 48);
-	result += addChar(c1, c2);
-	temp[cLoop] = (result % 10) + 48;
-	if (result > 9)
-	{
-	temp[cLoop + 1] = (result / 10) + '0';
+		m = r[k];
+		r[k] = r[l];
+		r[l] = m;
 	}
-	}
-	rcLoop = size_r - 1;
-	for (cLoop = 0; cLoop < (size_r + 1) / 2; cLoop++)
-	{
-	if (temp[rcLoop - cLoop] == '0' && next == 0)
-	{
-	rcLoop--;
-	cLoop--;
-	continue;
-	}
-	else
-	{
-	next = 1;
-	c1 = temp[cLoop];
-	r[cLoop] = temp[rcLoop - cLoop];
-	r[rcLoop - cLoop] = c1;
-	}
-	}
-	if (r[size_r - 1] != '\0')
-	return (0);
 	return (r);
 }
+
