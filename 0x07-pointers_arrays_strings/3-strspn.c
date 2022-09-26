08ea09164929f7fa676d1120f1287988d347bff0
emmanuel tofa <emmanuelutofa@gmail.com>
2022-09-26 06:56:14 -0700

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: segment to compare
+ * @accept: string
+ * Return: number of bytes of inittial segment
+ */
+
+unsigned int _strspn(char *s, char *accept)
+	{
+	unsigned int i, j;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+	j = 0;
+	while (accept[j] != '\0' && s[i] != accept[j])
+	j++;
+	if (accept[j] == '\0')
+	return (i);
+	i++;
+	}
+	return (i);
+}
