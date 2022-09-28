3916e155591abf4195feea084c0e77f3d4fd2631
emmanuel tofa <emmanuelutofa@gmail.com>
2022-09-28 01:41:21 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,20 @@
 #include "main.h"
+
 /**
- *_puts_recursion - prints a string
- *@s: pointer block of memory to fill
- *Return: void
+ * _puts_recursion - print a string.
+ * @s: pointer to char
+ * Return: No.
  */
 
 void _puts_recursion(char *s)
-
 {
-if (*s == '\0')
-{
-_putchar('\n');
-return;
-}
-
-_putchar (*s);
-_puts_recursion(s + 1);
-
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+	_putchar('\n');
+	}
 }
