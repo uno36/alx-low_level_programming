d970d12bc8b48e3c4465b6e6695b29859b3c32b8
emmanuel tofa <emmanuelutofa@gmail.com>
2022-10-14 15:25:54 -0700

    One woman's constant is another woman's variable

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,40 +1,25 @@
-#include <stdio.h>
 #include "variadic_functions.h"
-
+#include <stdarg.h>
+#include <stdio.h>
 /**
-  * print_strings - ...
-  * @separator: ...
-  * @n: ...
-  *
-  * Return: ...
+  * print_strings - print a list of strings
+  * @separator: characters used to delimit
+  * @n: nmber of items to print
   */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list args;
-	unsigned int i = 0;
-	char *string;
+	unsigned int i;
+	char *words;
+	va_list ap;
 
-	if (n > 0)
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
 	{
-		va_start(args, n);
-
-		while (i < n)
-		{
-			string = va_arg(args, char *);
-			if (string == NULL)
-				printf("%s", "(nil)");
-			else
-				printf("%s", string);
-
-			if (i != n - 1 && separator != NULL)
-				printf("%s", separator);
-
-			i++;
-		}
-
-		va_end(args);
+		words = va_arg(ap, char *);
+		(words) ? printf("%s", words) : printf("(nil)");
+		if (separator != NULL && i < n - 1)
+			printf("%s", separator);
 	}
-
 	printf("\n");
+	va_end(ap);
 }
-
